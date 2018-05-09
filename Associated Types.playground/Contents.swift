c9d34c6a7a07c1db62e09a16e24501139197d34f
Bob Godwin <bobgodwinx@gmail.com>
2018-05-09 14:42:24 +0200

    PATs: Initial introduction of  class which is a  wrapper

--- Associated Types.playground/Contents.swift
+++ Associated Types.playground/Contents.swift
@@ -12,3 +12,15 @@ protocol Row {
 struct Product { }
 /// Concrete Type `Item`
 struct Item { }
+/// Wrapper `AnyRow 
+class AnyRow<I>: Row {
+    private let configureClosure: (I) -> Void
+
+    init<T: Row>(_ row: T) where T.Model == I {
+        configureClosure = row.configure
+    }
+
+    func configure(with model: I) {
+        configureClosure(model)
+    }
+}
