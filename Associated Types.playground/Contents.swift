816ff7548b0072259a63bf398c178736d7df1842
Bob Godwin <bobgodwinx@gmail.com>
2018-05-09 12:53:31 +0200

    PATs: Initial introduction of `Row` interface with `associatedtype` Model

--- Associated Types.playground/Contents.swift
+++ Associated Types.playground/Contents.swift
@@ -1,1 +1,10 @@
 import Foundation
+import UIKit
+
+/// Rows `Interface`
+protocol Row {
+    ///PAT Placeholder for unknown Concrete Type `Model`
+    associatedtype Model
+    /// - Recieves a parameter of Concrete Type `Model`
+    func configure(with model: Model)
+}
