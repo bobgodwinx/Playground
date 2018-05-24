3000a080750914b259042a4fdfa56ff1c08cdffc
Bob Godwin <bobgodwinx@gmail.com>
2018-05-24 18:26:24 +0200

    S4D: Initial introduction of `TableDatasource` a generic rx-wrapper for `UITableViewDatasource`

--- DictionarySwift4.playground/Contents.swift
+++ DictionarySwift4.playground/Contents.swift
@@ -19,8 +19,55 @@ struct Person {
     let age: Int
     let nationality: Nationality
 }
+/// `TableDatasource`
+class TableDatasource: NSObject, UITableViewDataSource, RxTableViewDataSourceType, SectionedViewDataSourceType, UITableViewDelegate {
+    struct Section {
+        let title: String
+        let rows: [TableRow]
 
+        init(title: String, rows: [TableRow]) {
+            self.title = title
+            self.rows = rows
+        }
+    }
 
+    private var _model: [Section] = []
+
+    typealias Element = [Section]
+    func tableView(_ tableView: UITableView, observedEvent: Event<[TableDatasource.Section]>) {
+        Binder(self) {form, sections in
+            form._model = sections
+            tableView.reloadData()
+            }
+            .on(observedEvent)
+    }
+
+    func model(at indexPath: IndexPath) throws -> Any {
+        return _model[indexPath.section].rows[indexPath.row]
+    }
+    //MARK: Table View Data Source
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return _model.count
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard section < _model.count else { return 0 }
+        return _model[section].rows.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let item = _model[indexPath.section].rows[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: item.cellId, for: indexPath)
+        item.configureCell(cell)
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        guard section < _model.count else { return nil }
+        return _model[section].title
+    }
+}
+/// `TableRow`
 protocol TableRow {
     var configureCell: (UITableViewCell) -> Void { get }
     static var cellIdentifier: String {get}
@@ -30,7 +77,6 @@ protocol TableRow {
 extension TableRow {
     var cellId: String { return Self.cellIdentifier }
 }
-
 
 /// `ContactViewController`
 class ContactViewController: UITableViewController {
