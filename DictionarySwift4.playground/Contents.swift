93bad18709d0636c1d31154fb38cb371103afb83
Bob Godwin <bobgodwinx@gmail.com>
2018-05-25 18:22:16 +0200

    S4D: Initial implementation for `ContactViewModel` + clean of tableview

--- DictionarySwift4.playground/Contents.swift
+++ DictionarySwift4.playground/Contents.swift
@@ -57,9 +57,12 @@ class TableDatasource: NSObject, UITableViewDataSource, RxTableViewDataSourceTyp
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let item = _model[indexPath.section].rows[indexPath.row]
-        let cell = tableView.dequeueReusableCell(withIdentifier: item.cellId, for: indexPath)
-        item.configureCell(cell)
-        return cell
+        /// In your App you should be using the storyboard to dequeue different cells
+        /// let cell = tableView.dequeueReusableCell(withIdentifier: item.cellId, for: indexPath)
+        /// for this example we going to use only `PersonCell` hope you get the trick
+        let _cell = PersonCell(style: .value2, reuseIdentifier: item.cellId)
+        item.configureCell(_cell)
+        return _cell
     }
 
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
@@ -71,69 +74,64 @@ class TableDatasource: NSObject, UITableViewDataSource, RxTableViewDataSourceTyp
 protocol TableRow {
     var configureCell: (UITableViewCell) -> Void { get }
     static var cellIdentifier: String {get}
-    static var nibName: String { get }
 }
 
 extension TableRow {
     var cellId: String { return Self.cellIdentifier }
 }
-/// `UITableView`
-extension UITableView {
-    func register(_ tableRows: [TableRow.Type]) {
-        tableRows.forEach { register($0) }
-    }
 
-    func register(_ row: TableRow.Type) {
-        let nib = UINib(nibName: row.nibName, bundle: nil)
-        register(nib, forCellReuseIdentifier: row.cellIdentifier)
-    }
-}
 /// `Contactable`
 protocol Contactable {
-    ///enter requirement
+    var datasource: Driver<[TableDatasource.Section]> {get}
 }
 /// `ContactViewModel`
 class ContactViewModel: Contactable {
-    /// implement requirement
+
+    let datasource: Driver<[TableDatasource.Section]>
+    /// `init`
+    init(_ provider: Observable<[Person]>) {
+        self.datasource = provider
+            .map { items -> [TableDatasource.Section] in
+                 /// groupedByNationality = [Nationality: [Person]]` ///new feature in swift 4.x
+                 /// let groupedByNationality  = Dictionary(grouping: items) { $0.nationality }
+                /// For this example we are going to use the `LastName`
+                /// `grouped = [Character: [Person]]` ///new feature in swift 4.x
+                let grouped = Dictionary(grouping: items) { $0.lastName.first! }
+                let keys = grouped.keys.sorted()
+                return keys.map { key -> TableDatasource.Section in
+                    let rows = grouped[key]?.compactMap(PersonRow.init) ?? []
+                    return TableDatasource.Section(title: String(key), rows: rows)
+                }
+            }
+            .asDriver(onErrorJustReturn: [])
+    }
 }
+
+
+let provider = Observable.of(contacts)
+let viewModel = ContactViewModel(provider)
+
 /// `ContactViewController`
-class ContactViewController: UITableViewController {
+class ContactViewController: UIViewController {
+    private let bag = DisposeBag()
 
-    /// Only Leader and the richest
-    /// Disclaimer this data is just
-    /// for example purposes. I am not
-    /// responsible for any incorrect
-    /// information. I hereby declare
-    /// that I should not be held liable.
-    let contacts = [
-        Person(firstName: "Angela", lastName: "Merkel", age: 64, nationality: .german),
-        Person(firstName: "Theresa", lastName: "May", age: 61, nationality: .british),
-        Person(firstName: "Xi", lastName: "Jinping", age: 65, nationality: .chinese),
-        Person(firstName: "Muhammadu", lastName: "Buhari", age: 75, nationality: .nigerian),
-        Person(firstName: "Aliko", lastName: "Dangote", age: 61, nationality: .nigerian),
-        Person(firstName: "Michele", lastName: "Ferrero", age: 93, nationality: .italian),
-        Person(firstName: "Jack", lastName: "Ma", age: 53, nationality: .chinese),
-        Person(firstName: "Silvio", lastName: "Berlusconi", age: 81, nationality: .italian),
-        Person(firstName: "Richard", lastName: "Brandson", age: 67, nationality: .british),
-        Person(firstName: "Georg", lastName: "Schaeffler", age: 53, nationality: .german),
-        ]
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
     }
 
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .value2, reuseIdentifier: nil)
-        cell.textLabel?.text = "firstName: \(indexPath.row)"
-        cell.detailTextLabel?.text = "lastName: \(indexPath.row)"
-        return cell
+    private func configure() {
+        let tableView = UITableView(frame: CGRect(x: 0, y: 0, width: 768, height: 1024), style: .plain)
+        tableView.register(PersonCell.self, forCellReuseIdentifier: PersonRow.cellIdentifier)
+        /// Here goes the magical binding
+        viewModel
+            .datasource
+            .drive(tableView.rx.items(dataSource: TableDatasource()))
+            .disposed(by: bag)
+        /// Adding the `UITableView` as subview
+        view.addSubview(tableView)
     }
-
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "Section \(section)"
-    }
-
 }
 
 // Present the view controller in the Live View window
-PlaygroundPage.current.liveView = ContactViewController(style: .plain)
+PlaygroundPage.current.liveView = ContactViewController()
