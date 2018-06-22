7805ebfdd03d017028cba5bc0e2a4c3a1a855302
Bob Godwin <bobgodwinx@gmail.com>
2018-06-22 09:00:06 +0200

    OST: Initial introduction of `RxMBProgressHUD`  to make `MBProgressHUD` reactive

--- RxSwiftObserverType.playground/Contents.swift
+++ RxSwiftObserverType.playground/Contents.swift
@@ -3,4 +3,12 @@
 import RxSwift
 import MBProgressHUD
 
+class RxMBProgressHUD {
 
+    static let shared = RxMBProgressHUD()
+
+    enum State {
+        case hide(view: UIView, animated: Bool)
+        case show(view: UIView, animated: Bool)
+    }
+}
