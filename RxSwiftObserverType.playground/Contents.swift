14da1fe4585d08541918671f5d44413a1b063fc5
Bob Godwin <bobgodwinx@gmail.com>
2018-06-23 14:13:40 +0200

    OST: Make enum `Associated Type` optional

--- RxSwiftObserverType.playground/Contents.swift
+++ RxSwiftObserverType.playground/Contents.swift
@@ -8,8 +8,8 @@ class RxMBProgressHUD: ObserverType {
     static let shared = RxMBProgressHUD()
 
     enum State {
-        case hide(view: UIView, animated: Bool)
-        case show(view: UIView, animated: Bool)
+        case hide(view: UIView?, animated: Bool)
+        case show(view: UIView?, animated: Bool)
     }
 
     typealias E = RxMBProgressHUD.State
@@ -18,9 +18,13 @@ class RxMBProgressHUD: ObserverType {
         guard case .next(let state) = event else { return }
         switch state {
         case .hide(let view, let animated):
-            MBProgressHUD.hide(for: view, animated: animated)
+            if let view = view {
+                MBProgressHUD.hide(for: view, animated: animated)
+            }
         case .show(let view, let animated):
-            MBProgressHUD.showAdded(to:view, animated: animated)
+            if let view = view {
+                MBProgressHUD.showAdded(to:view, animated: animated)
+            }
         }
     }
 }
